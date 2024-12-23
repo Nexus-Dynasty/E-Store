4610c0af1c3adbe3d4c47129b72925031f89e160
King Harry <135327588+KingHarry001@users.noreply.github.com>
2024-12-23 18:49:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,411 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { myFunction() };
+
+    function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }
+
+const products = [
+  {
+    name: 'Sony Playstation 5',
+    url: 'https://i.ibb.co/NnD81dc/1721641928198.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'https://i.ibb.co/LvkHz0k/download-10.jpg',
+    category: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Cannon EOS Camera',
+    url: 'https://i.ibb.co/mhm1hLq/cannon-eos-camera.png',
+    category: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'https://i.ibb.co/LS9TDLN/sony-a7-camera.png',
+    category: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'https://i.ibb.co/QHgFnHk/lg-tv.png',
+    category: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'https://i.ibb.co/L0L9SdG/nintendo-switch.png',
+    category: 'games',
+    price: 299.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'https://i.ibb.co/C8rBVdT/xbox-series-x.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung TV',
+    url: 'https://i.ibb.co/Pj1nm4B/samsung-tv.png',
+    category: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Google Pixel',
+    url: 'https://i.ibb.co/5F58zmH/google-pixel.png',
+    category: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Sony ZV1F Camera',
+    url: 'https://i.ibb.co/5Wy3RZ9/sony-zv1f-camera.png',
+    category: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'https://i.ibb.co/FxM6rXq/toshiba-tv.png',
+    category: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'iPhone 15',
+    url: 'https://i.ibb.co/5vc7J6s/iphone-14.png',
+    category: 'smartphones',
+    price: 999.99,
+  },
+  {
+    name: 'Oriamo Led Light Headset',
+    url: 'https://i.ibb.co/NnD81dc/1721641928198.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'https://i.ibb.co/LvkHz0k/download-10.jpg',
+    category: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Cannon EOS Camera',
+    url: 'https://i.ibb.co/mhm1hLq/cannon-eos-camera.png',
+    category: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'https://i.ibb.co/LS9TDLN/sony-a7-camera.png',
+    category: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'https://i.ibb.co/QHgFnHk/lg-tv.png',
+    category: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'https://i.ibb.co/L0L9SdG/nintendo-switch.png',
+    category: 'games',
+    price: 299.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'https://i.ibb.co/C8rBVdT/xbox-series-x.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung TV',
+    url: 'https://i.ibb.co/Pj1nm4B/samsung-tv.png',
+    category: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Google Pixel',
+    url: 'https://i.ibb.co/5F58zmH/google-pixel.png',
+    category: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Sony ZV1F Camera',
+    url: 'https://i.ibb.co/5Wy3RZ9/sony-zv1f-camera.png',
+    category: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'https://i.ibb.co/FxM6rXq/toshiba-tv.png',
+    category: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'iPhone 15',
+    url: 'https://i.ibb.co/5vc7J6s/iphone-14.png',
+    category: 'smartphones',
+    price: 999.99,
+  },
+  {
+    name: 'Sony Playstation 5',
+    url: 'https://i.ibb.co/NnD81dc/1721641928198.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'https://i.ibb.co/LvkHz0k/download-10.jpg',
+    category: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Cannon EOS Camera',
+    url: 'https://i.ibb.co/mhm1hLq/cannon-eos-camera.png',
+    category: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'https://i.ibb.co/LS9TDLN/sony-a7-camera.png',
+    category: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'https://i.ibb.co/QHgFnHk/lg-tv.png',
+    category: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'https://i.ibb.co/L0L9SdG/nintendo-switch.png',
+    category: 'games',
+    price: 299.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'https://i.ibb.co/C8rBVdT/xbox-series-x.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung TV',
+    url: 'https://i.ibb.co/Pj1nm4B/samsung-tv.png',
+    category: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Google Pixel',
+    url: 'https://i.ibb.co/5F58zmH/google-pixel.png',
+    category: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Sony ZV1F Camera',
+    url: 'https://i.ibb.co/5Wy3RZ9/sony-zv1f-camera.png',
+    category: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'https://i.ibb.co/FxM6rXq/toshiba-tv.png',
+    category: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'iPhone 15',
+    url: 'https://i.ibb.co/5vc7J6s/iphone-14.png',
+    category: 'smartphones',
+    price: 999.99,
+  },
+  {
+    name: 'Oriamo Led Light Headset',
+    url: 'https://i.ibb.co/NnD81dc/1721641928198.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'https://i.ibb.co/LvkHz0k/download-10.jpg',
+    category: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Cannon EOS Camera',
+    url: 'https://i.ibb.co/mhm1hLq/cannon-eos-camera.png',
+    category: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'https://i.ibb.co/LS9TDLN/sony-a7-camera.png',
+    category: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'https://i.ibb.co/QHgFnHk/lg-tv.png',
+    category: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'https://i.ibb.co/L0L9SdG/nintendo-switch.png',
+    category: 'games',
+    price: 299.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'https://i.ibb.co/C8rBVdT/xbox-series-x.png',
+    category: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung TV',
+    url: 'https://i.ibb.co/Pj1nm4B/samsung-tv.png',
+    category: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Google Pixel',
+    url: 'https://i.ibb.co/5F58zmH/google-pixel.png',
+    category: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Sony ZV1F Camera',
+    url: 'https://i.ibb.co/5Wy3RZ9/sony-zv1f-camera.png',
+    category: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'https://i.ibb.co/FxM6rXq/toshiba-tv.png',
+    category: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'iPhone 15',
+    url: 'https://i.ibb.co/5vc7J6s/iphone-14.png',
+    category: 'smartphones',
+    price: 999.99,
+  },
+];
+
+// Get DOM elements
+const productsWrapper = document.getElementById('products-wrapper');
+const checkboxes = document.querySelectorAll('.check');
+const filtersContainer = document.getElementById('filters-container');
+const searchInput = document.getElementById('search');
+const cartButton = document.getElementById('cart-button');
+const cartCount = document.getElementById('cart-count');
+
+// Initialize cart item count
+let cartItemCount = 0;
+
+// Initialize products
+const productElements = [];
+
+// Loop over the products and create the product elements
+products.forEach((product) => {
+  const productElement = createProductElement(product);
+  productElements.push(productElement);
+  productsWrapper.appendChild(productElement);
+});
+
+// Add filter event listeners
+filtersContainer.addEventListener('change', filterProducts);
+searchInput.addEventListener('input', filterProducts);
+
+// Create product element
+function createProductElement(product) {
+  const productElement = document.createElement('div');
+
+  productElement.className = 'item space-y-2';
+
+  productElement.innerHTML = `<div
+  class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl"
+>
+  <i class="bi bi-heart fav"></i>
+
+  <a href="product.html">
+    <img
+    src="${product.url}"
+    alt="${product.name}"
+    class="w-full h-full object-cover"
+    />
+  </a>
+  <button class="status submit bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
+    >Add To Cart</button
+  >
+</div>
+<p class="text-xl">${product.name}</p>
+<strong><small class="highlight">$</small>${product.price.toLocaleString()}</strong>`;
+
+  productElement
+    .querySelector('.status')
+    .addEventListener('click', updateCart);
+
+  return productElement;
+}
+
+// Toggle add/remove from cart
+function updateCart(e) {
+  const statusEl = e.target;
+
+  if (statusEl.classList.contains('added')) {
+    // Remove from cart
+    statusEl.classList.remove('added');
+    statusEl.innerText = 'Add To Cart';
+    statusEl.classList.remove('bg-red-600');
+    statusEl.classList.add('bg-gray-800');
+
+    cartItemCount--;
+  } else {
+    // Add to cart
+    statusEl.classList.add('added');
+    statusEl.innerText = 'Remove From Cart';
+    statusEl.classList.remove('bg-gray-800');
+    statusEl.classList.add('bg-red-600');
+
+    cartItemCount++;
+  }
+
+  // Update cart item count
+  cartCount.innerText = cartItemCount.toString();
+}
+
+// Filter products by search or checkbox
+function filterProducts() {
+  // Get search term
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  // Get checked categories
+  const checkedCategories = Array.from(checkboxes)
+    .filter((check) => check.checked)
+    .map((check) => check.id);
+
+  // Loop over products and check for matches
+  productElements.forEach((productElement, index) => {
+    const product = products[index];
+
+    // Check to see if product matches the search or checked items
+    const matchesSearchTerm = product.name.toLowerCase().includes(searchTerm);
+    const isInCheckedCategory =
+      checkedCategories.length === 0 ||
+      checkedCategories.includes(product.category);
+
+    // Show or hide product based on matches
+    if (matchesSearchTerm && isInCheckedCategory) {
+      productElement.classList.remove('hidden');
+    } else {
+      productElement.classList.add('hidden');
+    }
+  });
+}
